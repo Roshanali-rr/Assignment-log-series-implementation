953476bd9175569921c718052998b028bf00950f
Roshanali-rr <63875910+Roshanali-rr@users.noreply.github.com>
2020-10-22 22:26:56 +0530

    Update log_series.s

--- log_series.s
+++ log_series.s
@@ -3,30 +3,31 @@
 	 ENTRY 
 __main  FUNCTION		         
     
-	VLDR.F32   s1, = 5
+    VLDR.F32   s1, = 5
     VLDR.F32   s2, = 5
     VLDR.F32   s5, = 5 
     VLDR.F32   s6, = 1	
     VLDR.F32   S4, = -1
-	VLDR.F32   S11, = -1
+    VLDR.F32   S11, = -1
     VLDR.F32   S12, = 1          
 
-	MOV R3,#100;
-	MOV R5,#5;
-	MOV R6,#0
+   MOV R3,#100;
+   MOV R5,#5;
+   MOV R6,#0
 	
 LOOP CMP R3,R6
-	BGE LOOP1
-	B stop
+     BGE LOOP1
+     B stop
 	
-LOOP1 VMOV.F32 S7, S1;
-	  VMUL.F32 S5,S5,S2;
-	  ADD R6,R6,#1;
-	  VADD.F32	s6,s6,s12;
+LOOP1 ADD R6,R6,#1;
+      VADD.F32	s6,s6,s12;
+      VMUL.F32 S5,S5,S2;
 	  VDIV.F32 s8,s5,s6;
 	  VMUL.F32 S9,S8,S4;
-	  VADD.F32	s1,s7,s9;
 	  VMUL.F32 S4,s4,s11;
+	  VMOV.F32 S7, S1;
+	  VADD.F32 s1,s7,s9;
+	  
 	  B LOOP;
 	  
 stop B stop 
