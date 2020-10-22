08e17949f700f3ff8272ab0183021707ce58c19c
Roshanali-rr <63875910+Roshanali-rr@users.noreply.github.com>
2020-10-22 21:15:46 +0530

    Update log_series.s

--- log_series.s
+++ log_series.s
@@ -1,14 +1,15 @@
-     area     appcode, CODE, READONLY
-     export __main
+ 	AREA     appcode, CODE, READONLY
+     EXPORT __main
 	 ENTRY 
-__main  function		 
-       VLDR.F32   s1, = 5
+__main  FUNCTION		         
+    
+	VLDR.F32   s1, = 5
        VLDR.F32   s2, = 5   
        VLDR.F32   S4, = -1
 	   VLDR.F32   S11, = -1
-     	          
+     	VLDR.F32   S12, = 1          
 
-	MOV R3,#64;
+	MOV R3,#100;
 	MOV R5,#5;
 	MOV R6,#0
 	
@@ -19,11 +20,12 @@ LOOP CMP R3,R6
 LOOP1 VABS.F32 S7, S1;
 	  VMUL.F32 S5,S5,S2;
 	  ADD R6,R6,#1;
+	  VADD.F32	s6,s6,s12;
 	  VDIV.F32 s8,s5,s6;
 	  VMUL.F32 S9,S8,S4;
 	  VADD.F32	s1,s7,s9;
 	  VMUL.F32 S4,s4,s11;
-	  B stop
+	  B l1;
 	  
 stop B stop 
    ENDFUNC
