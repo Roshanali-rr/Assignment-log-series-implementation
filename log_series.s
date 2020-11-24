c3cdc2620db48d6558a0f3544446db02468b786d
Roshanali-rr <63875910+Roshanali-rr@users.noreply.github.com>
2020-11-24 15:10:04 +0530

    Update log_series.s

--- log_series.s
+++ log_series.s
@@ -7,110 +7,39 @@
 __main  FUNCTION	
 
  
-    VLDR.F32   s6, = 1
+    VLDR.F32   s1, = 5
+    VLDR.F32   s1, = 5 
+    VLDR.F32   s2, = 5
+    VLDR.F32   s5, = 5 
+    VLDR.F32   s6, = 1	
+    VLDR.F32   S4, = -1
+    VLDR.F32   S11, = -1
     VLDR.F32   S12, = 1          
-   MOV R11, #2; FOR SWITCHING BETWEEN LOGIC GATES 
-   MOV R7,#0 ; INPUT X1
-   MOV R8,#1 ; INPUT X2 
-   MOV R9,#1; INPUT X3
-   MOV R10,#1 ; BIAS 
-   MOV R3,#100; N=100 FOR 10 iterations
-   MOV R12,#0 ; r12 and S6 are used as a counter to count 100 iterations 
-   
 
-LOGIC_AND CMP R11, #0;
-          BNE LOGIC_OR;
-          MOV R2, #-3;
-          MOV R4, #2;
-    	  MOV R5, #2;	  
-          MOV R6, #-6;
-		   B COMPUTE
-		  
-LOGIC_OR  CMP R11, #1;
-          BNE LOGIC_NOT;
-          MOV R2, #2;
-          MOV R4, #2;
-    	  MOV R5, #2;	  
-          MOV R6, #-1;
-		  B COMPUTE
+   MOV R3,#100;
+   MOV R3,#100; N=100 FOR 100 iterations
+   MOV R5,#5;
+   MOV R6,#0
+   MOV R6,#0 ; r6 and s6 are used as a counter to count 100 iterations 
 
-LOGIC_NOT CMP R11, #2;
-          BNE LOGIC_NAND;
-          MOV R2, #-7;
-          MOV R4, #2;
-    	  MOV R5, #2;	  
-          MOV R6, #2;
-		   B COMPUTE
-		  
-LOGIC_NAND CMP R11, #3;
-          BNE LOGIC_NOR; 
-          MOV R2, #-2;
-          MOV R4, #-2;
-    	  MOV R5, #-2;	  
-          MOV R6, #5;
-		   B COMPUTE
-
-LOGIC_NOR CMP R11, #4;
-          BNE LOGIC_XOR;
-          MOV R2, #-2;
-          MOV R4, #-2;
-    	  MOV R5, #-2;	  
-          MOV R6, #1;
-		   B COMPUTE
-		  
-LOGIC_XOR CMP R11, #6;
-          BNE LOGIC_XNOR;
-          MOV R2, #-5;
-          MOV R4, #2;
-    	  MOV R5, #1;	  
-          MOV R6, #1;
-		   B COMPUTE	
-
-LOGIC_XNOR MOV R2, #-5;
-          MOV R4, #2;
-    	  MOV R5, #1;	  
-          MOV R6, #1;
-		  B COMPUTE
-
-COMPUTE   MUL R2,R2,R7;
-		  MUL R4,R4,R8;
-		  MUL R5,R5,R9;
-		  MUL R6,R6,R10;
-		  ADD R2,R2,R4;
-		  ADD R5,R5,R6;
-		  ADD R1,R2,R5;
-		  VMOV.F32 S1,R1;
-		  VMOV.F32 S2,R1;
-		  VMOV.F32 S5,R1;
-		  VCVT.F32.S32 S1,S1;
-		  VCVT.F32.S32 S2,S2;
-		  VCVT.F32.S32 S5,S5;
-          B LOOP
-	
-LOOP CMP R3,R12
+LOOP CMP R3,R6
+     BGE LOOP1
      BGE LOOP1 ; till 100 iterations it will move to LOOP1 and after that it go to STOP
-     BLE LOOP2
-	 B stop
-	
-LOOP1 ADD R12,R12,#1; r12 and s6 are incremented after every iteration
+     B stop
+
+LOOP1 ADD R6,R6,#1;
+LOOP1 ADD R6,R6,#1; r6 and s6 are incremented after every iteration
       VADD.F32	s6,s6,s12;
       VMUL.F32 S5,S5,S2;
-      VDIV.F32 s8,s5,s6;
-	  VADD.F32 S1,S1,S8;
-	  VDIV.F32 s5,s5,s6;
-	  B LOOP;
+	  VDIV.F32 s8,s5,s6;
+	  VMUL.F32 S9,S8,S4;
+	  VMUL.F32 S4,s4,s11;
+	  VMOV.F32 S7, S1;
+	  VADD.F32 s1,s7,s9;
+	  VADD.F32 s1,s7,s9; s1 is the ln(1+x) value with x=5
 
-LOOP2 VADD.F32 S1,S1,S12;
-      VADD.F32 S2,S1,S12;
-	  VDIV.F32 s1,s1,s2;
-	  VCVTR.U32.F32 S1,S1;
-	  VMOV.F32 R0,D0[1]
-	  BL printMsg
-       B stop
-      
+	  B LOOP;
 
 stop B stop 
    ENDFUNC
    END
-	   
-	
